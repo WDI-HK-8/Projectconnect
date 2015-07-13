81926741170cc0d0532f3ae914ee48340f532f02
JEFFREY <monogut@gmail.com>
2015-07-13 14:33:01 +0800

    initial commit

---
+++ js/application.js
@@ -0,0 +1,22 @@
+ /*create board please note that the idea and
+design of the template was borrowed from 
+ http://www.codecademy.com/Smwaters/codebits/Uln4W/edit */
+$(document).ready(function(){
+
+var columns = [[], [], [], [], [], [] ,[]];
+// seven columns 
+var tokens = 0
+var playing = true;
+var started = false;
+var maxHeight = 6;
+
+// set up board and switch player by switching the color
+// of the token
+// set up dropping token into each column when button is pressed  
+
+
+
+
+    
+})
+
