23763c9e524b387bbac0c4db106dd8b31d04ae5f
JEFFREY <monogut@gmail.com>
2015-07-15 17:03:39 +0800

    javascript updated, jquery actions commented out for later use

--- js/application.js
+++ js/application.js
@@ -1,187 +1,173 @@
-
-
-//  /*create board please note that the idea and
-// design of the template was borrowed from 
-//  http://www.codecademy.com/Smwaters/codebits/Uln4W/edit */
-// /*control j for js lint*/
 "use strict";
 
-// var board  =  [ [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-//                 [0,0,0,0,0,0]
-// ];
+function Player(id) {
+  this.id   = id;
+}
 
-// seven columns, six rows 
-var tokens = 0
-var playing = true;
-var started = false;
-var maxHeight = 6;
-var winner = false; 
-var gameOver = false;
-var playersTurn = true;
-var victor = false;
-var player1 = 1;
-var player2 = 2;
+// Constructor function will create new Game objectss
+function Game() {
+  // seven columns, six rows
+  this.board  =  [[0,0,0,0,0,0],     // column 0
+                  [0,0,0,0,0,0],     // column 1
+                  [0,0,0,0,0,0],     // column 2
+                  [0,0,0,0,0,0],     // column 3
+                  [0,0,0,0,0,0],     // column 4
+                  [0,0,0,0,0,0],     // column 5
+                  [0,0,0,0,0,0]];    // column 6
+}
 
-$(document).ready(function() {
+// Game objects will have a resetBoard() method
+Game.prototype.resetBoard = function() {
+  this.board  =  [[0,0,0,0,0,0],
+                  [0,0,0,0,0,0],
+                  [0,0,0,0,0,0],
+                  [0,0,0,0,0,0],
+                  [0,0,0,0,0,0],
+                  [0,0,0,0,0,0],
+                  [0,0,0,0,0,0]];
+}
 
-   //  $('.button reset').click(function(){
-   //  board = [
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //              [0,0,0,0,0,0]
-   //  ];
-   // playersTurn = true; 
-   // gameOver = false; 
-   //  });
+Game.prototype.insertToken = function(player, column) {
+  var row = 0;
+  while (this.board[column][row] > 0 && row < 6) {
+    row++
+  }
+  this.board[column][row] = player.id;
+}
 
-  $('.button').mouseenter(function(){
-    $(this).addClass("highlighted");
-  });
+// This function will return true if there are any 4 consecutive elements
+// either horizontally, vertically, or diagonally
+Game.prototype.checkWinner = function() {
+  return (this.checkVertical());
+  // return (this.checkHorizontal() || this.checkVertical() || this.checkDiagonal());
+}
+
+// This function will return true if there are 4 consecutive vertical
+// elements in the board
+
+/*-----4 consecutive vertical elements----------*/
+
+Game.prototype.checkVertical = function() {
+  var foundFourConnected = false;
+  var column = 0;
+
+  while (column<7 && !foundFourConnected) {
+    if (this.checkVerticalArray(this.board[column])) {
+      foundFourConnected = true;
+    } else {
+      column++;
+    }
+  }
+
+  return foundFourConnected;
+}
+/*-----4 consecutive horizontal elements----------*/
+
+Game.prototype.checkHorizontal = function() {
+  var foundFourConnected = false;
+  var row = 0; 
+
+  while (row<8 &&!foundFourConnected) {
+    if (this.checkHorizontalArray(this.board[row])) {
+      foundFourConnected = true;
+    } else {
+      row++;
+    }
+  }
+
+  return foundFourConnected;
+}
+
+/*-----4 consecutive diagonal elements----------*/
+
+Game.prototype.checkDiagonal = function() {
+  var foundFourConnected = false;
+  var row = 0;
+  var column = 0;
+
+   while (row < 8 && column <7 && !foundFourConnected) {
+       if (this.checkDiagonalArray(this.board[column][row])) {
+        foundFourConnected = true; 
+  }   else {
+        diagonal++;
+      }
+  }
+} 
+
+
+/*----------check vertical array------*/
+// This function will return true if there are 4 consecutive elements
+// row and column in the array
+Game.prototype.checkVerticalArray = function(element, column) {
   
-  $('.button').mouseleave(function(){
-    $(this).removeClass("highlighted");
-  }); 
+  var element = 0;
+  var player1 = 1;
+  var player2 = 2; 
 
-  $(".button").click(function(){
-    $(".displaycontainer .token").toggleClass("player1");
-    $(".displaycontainer .token").toggleClass("player2");
+  while (column < 8 && element = 4) {
+     if (this.board[column][element])){
+     foundFourConnected = true;
+} 
+  return element;
+  }
+}
 
-  });
+/*----------check horizontal array-----------------*/
+Game.prototype.checkHorizontalArray = function(element, row) {
   
+  var element = 0;
+  var player1 = 1;
+  var player2 = 2; 
 
-});
-   
+  while (row < 7 && element = 4) {
+     if (this.board[row][element])){
+     foundFourConnected = true;
+} 
+  // YOUR CODE HERE
+  return element;
+  }
+}
+/*----------check diagonal array(NEED HELP)-----------------*/
+Game.prototype.checkDiagonalArray = function(element, diagonal) {
+
+  var element = 0; 
+  var player1 = 1;
+  var player2 = 2; 
+  curCol = column + 1;
+  curRow = row + 1;
+  countBack = 0
+  countForward = 0
 
 
-//     ]
+  while (curRow < 7 && curCol < 8 && board[curRow][curCol] == player) {
+      backCount++
+      curRow--;
+      curCol--;
+  }
 
-// })
+  backCount = 0;
+  forwardCount = 0;
+  curCol = column - 1;
+  curRow = row - 1;
+}
 
-
-// })
-
-// var droptoken = function(){
-//     if 
-//         $(".displaycontainer .token").click(function(){
-//             var results = columns.filter(function(x) { return x.ID == id });
-//         })
-// }
-
-// /*define each column, each column's b,c,d,f should be excluded*/
-
-// For columns( i= 0; i<7; i++)
-
-// var 
 
 // $(document).ready(function() {
 
-// /* set up board and alternate player by switching the color
-//  of the token when button is prressed */
-// /* set up dropping token into each column when button 
-// is pressed*/  
 
-// var updateDisplayToken = function() {
-//     if $(".displaycontainer .token").click(function(){
-     
 
-//     })
-//     if($("html").hasClass("ie"))
-//      $(".deleteme").remove();
-//   $('.displaycontainer.token').click(function(){
-//   $('.displaycontainer.token').fadeOut('slow');
-//   });
+//   $('.button').mouseenter(function(){
+//     $(this).addClass("highlighted");
+   
 //   });
   
+//   $('.button').mouseleave(function(){
+//     $(this).removeClass("highlighted");
+//   }); 
 
-
-// // var switchPlayer = function() {
-// //     if(started = true) return;
-// //     tokens++;
-// //     updateDisplayToken();
-// // };
-
-// //   /* tokens in lowest row only*/ 
-
-
-// // var updateDisplayToken = function() {
-// //     $(".displaycontainer .token").addClass("deleteme");
-// //     if($("html").hasClass("ie"))
-// //      $(".deleteme").remove();
-// //     else
-// //         $(".deleteme").effect("fade", function(){
-// //             $(".deleteme").remove();}); 
-// //     var player = token%2 ? "player1" : "player2";
-// //     var newToken = "<div style=\"display:none\"class=\"token"
-// //        + p + "\"></div>";
-// //        $('.displaycontainer > div').prepend($(newToken));
-// //        if(!$("html").hasClass("ie"))
-// //          $("displaycontainer.token")
-// //               .css("position", "absolute");
-// //         $('.displaycontainer.token').show("slide");
-// //         $('.displaycontainer.token').click(switchPlayer);
-// // };
+//   $(".button").click(function(){
+//     $(".displaycontainer .token").toggleClass("player1");
+//     $(".displaycontainer .token").toggleClass("player2");
 
 
 
-
-// //         if(!$("html").hasClass("ie"))
-// //             $(".displaycontainer .token")
-// //                 .css("position", "absolute");
-
-// //     $(".columncontainer .button").click(function(){
-// //         var index = $(this).parent().index();
-// //         if(!updateGame(index)) return;
-// //         started = true;
-// //         var player = tokens%2 ? "player2" : "player1";
-// //         var newToken = "<div class=\"token " + p + "\"></div>";
-// //         $(this).prev().prepend(newToken);
-// //         var t = $(this).prev()
-// //             .children(".token:first-child").position.top;
-// //         $(this).prev()
-// //              .children(".token:first-child").css("top",t);
-// //         if($("html")).hasClass("ie"))
-// //            $(this).prev().children(".token:first-child")
-// //                 .animate({top:"+="+dropLength(index)+"px"}, 300);
-// //         if(!playing) return;
-// //         updateDisplayToken();
-// //     });
-    
-
-// //     $(".reset").click(function() {
-// //         $(".gamecontainer.token").fadeOut(function()
-// //             {$(this).remove();});
-// //     for (var i = 0; i < columns.length; i++) columns[i] = [];
-// //         $(".win").hide();
-// //         $("displaycontainer h2").text("winner!");
-// //     playing = true;
-// //     started = false;
-// //     updateDisplayToken();
-// // });
-
-// //  $(".displaycontainer.token").click(switchPlayer);
-// //     $("button").mouseenter(function(){
-// //         $(this).addClass("highlighted");
-// //     });
-// //     $(".button").mouseleave(function(){
-// //         $(this).removeClass("highlighted");
-// //     });
-// // });
-
-
-
-
-
-
-
-
-
-//  
