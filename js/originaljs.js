7ec5f280c282e4dfd388fb72b45d2d8f45dd84c7
JEFFREY <monogut@gmail.com>
2015-07-20 09:35:47 +0800

    modified html, css, some javascript, stuck with connected jquery to make the whole thing work...

--- js/originaljs.js
+++ js/originaljs.js
@@ -1,17 +1,10 @@
-//when the mouse hovers over a column, a token on top is displayed, when a column is clicked, 
-//the token is drooped into the lowest row 
-
-
-
-
-
 "use strict";
 
 function Player(id) {
   this.id = id;
 }
 
-// Constructor function will create new Game objects
+// Constructor function will create new Game objectss
 function Game() {
   // seven columns, six rows
   this.board  =  [[0,0,0,0,0,0],     // column 0
@@ -21,17 +14,6 @@ function Game() {
                   [0,0,0,0,0,0],     // column 4
                   [0,0,0,0,0,0],     // column 5
                   [0,0,0,0,0,0]];    // column 6
-
-
-
-  var column1 = board[0]
-  var column2 = board[1]
-  var column3 = board[2]
-  var column4 = board[3]
-  var column5 = board[4]
-  var column6 = board[5]
-  var column7 = board[6]
-
 }
 
 // Game objects will have a resetBoard() method
@@ -58,7 +40,7 @@ Game.prototype.insertToken = function(player, column) {
 Game.prototype.checkWinner = function() {
   return (this.checkHorizontal() || this.checkVertical() || this.checkDiagonal());
 }
-$('#declwinner').css.remove('display:none')
+
 // This function will return true if there are 4 consecutive vertical
 // elements in the board
 
@@ -171,42 +153,8 @@ $(document).ready(fuction(){
 
 $(#column1).hover('highlighted'){
 
-  $(document).ready(fuction(){
-
-$(.column).click(function(){
-  $('.displaytoken').toggleclass(.player2);
-  $('.hovertoken').toggleclass(.player2);
-  $(".emptytoken").remove();
-  $('token').toggleclass(.player2);
   
-
-
-  var player1 = 1
-  var player2 = 2
-
-  for (var i=0; i =< 7; i++){
-    change column1[0] to 1 or 2, then change column[1] to 1 or 2
-     alternate between 1 and 2 for each array until filled
-
-  }
-
-if 4 in a row, input text winner
-  add one to the player's score 
-
-})
-
-
-$('#column1').hover('highlighted')
-$('#column2').hover('highlighted')
-$('#column3').hover('highlighted')
-$('#column4').hover('highlighted')
-$('#column5').hover('highlighted')
-$('#column6').hover('highlighted')
-$('#column7').hover('highlighted')
-
-})
 }
 
   
 })
-
